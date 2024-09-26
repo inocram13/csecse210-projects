3f4588fb6f87d081b2d87e133d52cc5852d62088
inocram13 <lol.ming113@gmail.com>
2024-09-26 10:33:55 +0800

    Team Activity

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,31 @@
+// Welcome to Journal Project
+// Please select one of the following choices.
+// 1. Write
+// 2.Display
+// 3. Load
+// 4.Save
+// 5. Quit
+// What would you like to do? 
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry) 
+    {
+        
+    }
+    public void DisplayAll()
+    {
+
+    }
+    public void SavetoFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
