4684cad855a7c1e78a769ff1dbf5be95116fe753
inocram13 <lol.ming113@gmail.com>
2024-10-01 22:29:35 +0800

    Fraction Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+       Fraction fraction1 = new Fraction();
+       Fraction fraction2 = new Fraction(6);
+       Fraction fraction3 = new Fraction(3,4);
+       Fraction fraction4 = new Fraction(1,3);
+
+    //    Display
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        // Setters
+        fraction3.SetTop(7);
+        fraction3.SetBottom(13);
+        // Getters
+        Console.WriteLine(fraction3.GetTop());
+        Console.WriteLine(fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
-}
+
+}   
