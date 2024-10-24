8622f592e3432ff8f07c596bb04569da99cf3fde
inocram13 <lol.ming113@gmail.com>
2024-10-24 22:06:29 +0800

    Polymorphism

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using shape_demo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square(4, "Red")); 
+        shapes.Add(new Rectangle(3, 5, "Blue"));
+        shapes.Add(new Circle(2, "Green")); 
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine();
+
+        }
+
     }
 }
---
+++ prepare/Learning06/square.cs
@@ -0,0 +1,16 @@
+namespace shape_demo
+{
+    public class Square : Shape
+    {
+        private double _side;
+        public Square(double side, string color) : base(color)
+        {
+            _side = side;
+        }
+
+        public  override double GetArea()
+        {
+            return _side * _side;
+        }
+    }
+}
